ca2cfdc29c3cfba132130052f833638d9437496c
Prashant Balotra <prashantbalotra@Prashants-MacBook-Air.local>
2025-06-14 15:10:50 +0530

    Project Setup

---
+++ src/main/java/com/codewithmonks/docmgmt/DocMgmtAppApplication.java
@@ -0,0 +1,13 @@
+package com.codewithmonks.docmgmt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DocMgmtAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DocMgmtAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/codewithmonks/docmgmt/DocMgmtAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codewithmonks.docmgmt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocMgmtAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
